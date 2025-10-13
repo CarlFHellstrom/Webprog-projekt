1c961eb7dd9352b66c7daa7216e8c7dc68b92f24
Carl Hellstrom <carl.f.hellstrom@gmail.com>
2025-10-13 14:44:33 +0200

    Fixed URL issuis in for SearchMedia and SearchResult

--- react-starter/src/App.tsx
+++ react-starter/src/App.tsx
@@ -1,8 +1,6 @@
 import { NavigationMenuContent, NavigationMenuLink, NavigationMenuTrigger, NavigationMenuItem, NavigationMenuList, NavigationMenu} from '@radix-ui/react-navigation-menu';
-import './styles/App.css';
-import './styles/App.css';
-import { Button } from './components/ui/button';
-import { Link, Outlet, useOutletContext } from "react-router";
+import './styles/App.css';      
+import { Link, Outlet, useOutletContext } from "react-router-dom";
 
 
 function App() {
@@ -11,8 +9,7 @@ function App() {
       <h1 className="text-3xl font-bold underline mb-2">
         Movie list
       </h1>
-      {NavigationMenuDemo()}
-
+      <NavigationMenuDemo />
       <div className="mt-6">
         <Outlet />
       </div>
